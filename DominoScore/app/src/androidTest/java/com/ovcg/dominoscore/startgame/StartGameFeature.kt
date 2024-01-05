0325fc1e1da15fbc8c2f3275f6766c9662abb69b
otavio <17763111+ovcg@users.noreply.github.com>
2024-01-05 13:58:20 -0300

    test: add start game fill players name's

--- DominoScore/app/src/androidTest/java/com/ovcg/dominoscore/startgame/StartGameFeature.kt
+++ DominoScore/app/src/androidTest/java/com/ovcg/dominoscore/startgame/StartGameFeature.kt
@@ -2,9 +2,11 @@ package com.ovcg.dominoscore.startgame
 
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.action.ViewActions.typeText
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
 import com.ovcg.dominoscore.utils.BaseUITest
@@ -17,12 +19,17 @@ import com.ovcg.dominoscore.R
 class StartGameFeature : BaseUITest() {
 
     @Test
-    fun displayFieldsToChoosePartners() {
+    fun displayFieldsAndFillWithPlayersNames() {
         navigateToStartGameScreen()
-        onView(withId(R.id.edt_first_player)).check(matches(isDisplayed()))
+        onView(withId(R.id.edt_first_player))
+            .check(matches(isDisplayed()))
+            .perform(typeText("Player1")).check(matches(withText("Player1")))
         onView(withId(R.id.edt_second_player)).check(matches(isDisplayed()))
+            .perform(typeText("Player2")).check(matches(withText("Player2")))
         onView(withId(R.id.edt_third_player)).check(matches(isDisplayed()))
+            .perform(typeText("Player3")).check(matches(withText("Player3")))
         onView(withId(R.id.edt_fourth_player)).check(matches(isDisplayed()))
+            .perform(typeText("Player4")).check(matches(withText("Player4")))
     }
 
     private fun navigateToStartGameScreen() {
